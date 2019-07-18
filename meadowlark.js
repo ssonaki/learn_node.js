09dceb4f616a9e468f235e2ef5b3e6daf1041f05
chalie <free7omove@gmail.com>
2019-07-18 21:28:29 +0900

    Add module로 분리

--- meadowlark.js
+++ meadowlark.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var handlebars = require('express-handlebars')
             .create({defaultLayout:'main'});
+var fortune = require('./lib/fortune');
+
 var app = express();
 
 app.use(express.static(__dirname + '/public'));
@@ -9,22 +11,13 @@ app.engine('handlebars', handlebars.engine);
 app.set('view engine', 'handlebars');
 
 
-var fortunes = [
-	"Conquer your fears or they will conquer you.",
-	"Rivers need springs.",
-	"Do not fear what you don't know.",
-	"You will have a pleasant surprise.",
-	"Whenever possible, keep it simple.",
-];
 
 app.get('/', function(req, res) {
     res.render('home');
 });
 
 app.get('/about', function(req, res) {
-    var randomFortune = 
-        fortunes[Math.floor(Math.random() * fortunes.length)];
-    res.render('about', {fortune: randomFortune});
+    res.render('about', {fortune: fortune.getfortune()});
 });
 
 app.use(function(req, res) {
